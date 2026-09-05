e7fc33878a21aaa7c78f6ab8aea4e1d3910aa25b
Suresh Menon <motiondynamic168@gmail.com>
2026-09-05 18:24:51 +0800

    Release regression testing as v0.4.0

--- agent_reliability/__init__.py
+++ agent_reliability/__init__.py
@@ -2,6 +2,7 @@
 
 from .core import Agent, EvaluationReport, Evaluator, evaluate_cases
 from .failures import Failure, FailureCategory, classify_failure, classify_failures
+from .regression import RegressionChange, RegressionReport, compare_reports
 from .tools import ToolCall, ToolRecorder, ToolTrace, evaluate_tool_calls
 
 __all__ = [
@@ -10,12 +11,15 @@ __all__ = [
     "Evaluator",
     "Failure",
     "FailureCategory",
+    "RegressionChange",
+    "RegressionReport",
     "ToolCall",
     "ToolRecorder",
     "ToolTrace",
     "classify_failure",
     "classify_failures",
+    "compare_reports",
     "evaluate_cases",
     "evaluate_tool_calls",
 ]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
